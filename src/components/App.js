85aeaea563cc4170868e96ac2f341f589f0fff2a
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-02 16:31:06 +0300

    added Navbar route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import '../stylesheets/App.css';
 import React,{ useEffect, useState } from 'react';
 import { Route,Routes } from 'react-router-dom';
+import Navbar from './Navbar';
 import Login from './login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Navbar /*currentUser={currentUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}*/ />
+      <Routes>
+        <Route
+        path="/login"
+        element={<Login />}
+        />
+        {/* <Route
+        path="/signup"
+        /> */}
+      </Routes>
       <Login />
     </div>
   );
