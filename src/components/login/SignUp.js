6207220d17fa9705b5a3b5a28a7b7eee1cb85ca3
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-05 18:43:58 +0300

    Created sign up feature

--- src/components/login/SignUp.js
+++ src/components/login/SignUp.js
@@ -1,62 +1,75 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "../../stylesheets/SignUp.css"
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../UserDataProvider";
 
 const API = "https://charity-users-db.vercel.app/users"
 
 function SignUp({users,setUsers}){
 
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
     const [rPassword,setRPassword] = useState('');
 
     const navigate = useNavigate();
+    const [user,setUser,API] = useContext(UserContext)
+
+    const input = {email:email,username:username,password:password,name:name}
     // const newId = users[users.length-1]?.id+1;
 
     // const newUser = {
-    //     id:newId,
     //     username:username,
-    //     password:password,
-    //     donations:[]
+    //     password:password
     // }
 
     function cancelClicked(){
         navigate("/login");
     }
 
-    // function signClicked(e){
-    //     e.preventDefault();
-    //     const newUsers = [...users,newUser]
-    //     const userFound = users.find(user=>user.username===username);
-
-    //     if(userFound){
-    //         alert('Username already exist')
-    //     }
-    //     else if(password!==rPassword){
-    //         alert('Confirm password inputs!')
-    //     }
-    //     else{
-    //         setUsers(newUsers);
-    //         navigate('/login')
-
-    //         fetch(API,{
-    //             method:'POST',
-    //             headers:{
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body:JSON.stringify(newUser)
-    //         })
-    //         .then(resp=>resp.json())
-    //         .then(data=>console.log(data))
-    //     }
-    // }
+    function signClicked(e){
+        e.preventDefault();
+        if(password!==rPassword){
+            alert('Confirm password inputs!')
+        }
+        else{
+            // navigate('/login')
+            fetch(`${API}/auth/register`,{
+                method:'POST',
+                body:JSON.stringify(input)
+            })
+            .then(resp=>resp.json())
+            .then(data=>{
+                if (data.data.message){
+                    window.alert(data.data.message)
+                }
+                else{
+                    window.alert("New User Created")
+                    navigate('/login')
+                }
+            })
+        }
+    }
 
     return(
-        <form /*onSubmit={(e)=>signClicked(e)}*/ style={{border:"1px solid #ccc"}}>
+        <form onSubmit={(e)=>signClicked(e)} style={{border:"1px solid #ccc"}}>
             <div className="container">
                 <h1>Sign Up</h1>
                 <p>Please fill in this form to create an account.</p>
                 <hr/>
+
+                <label htmlFor="name"><b>Full Name</b></label>
+                <input
+                onChange={(e)=>setName(e.target.value)}
+                value={name}
+                type="text" placeholder="Enter Full Name" name="name" required/>
+
+                <label htmlFor="email"><b>Email</b></label>
+                <input
+                onChange={(e)=>setEmail(e.target.value)}
+                value={email}
+                type="text" placeholder="Enter Email" name="email" required/>
 
                 <label htmlFor="username"><b>Username</b></label>
                 <input
