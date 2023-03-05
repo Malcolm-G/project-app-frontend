5923216928d64a3c00ebeb947f6288cd29ac7f51
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-05 21:39:30 +0300

    fetched user's projects

--- src/components/UserDataProvider.js
+++ src/components/UserDataProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useEffect, useState } from "react"
 
 // create the context object
 const UserContext = createContext()
@@ -7,7 +7,20 @@ const API = 'http://127.0.0.1:9292'
 // create the context provider (component)
 function UserDataProvider({ children }) {
     const [user, setUser] = useState(null)
-    
+    const [projects, setProjects] = useState({})
+
+    useEffect(()=>{
+        if(user){
+            fetch(`${API}/projects/${user.id}`)
+            .then(resp=>resp.json())
+            .then(data=>{
+                // console.log(...data)
+                setProjects((projects)=>data)
+                // console.log(projects)
+            })
+        }
+    },[user])
+    // console.log(projects)
     const value = [user, setUser,API]
     
     return (
